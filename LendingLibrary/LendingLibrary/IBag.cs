4616053c2707ec561d90b7eaad65db02b711617f
IslamAlsawaftah <islamalsawaftah99@gmail.com>
2022-04-05 14:10:42 +0300

    add project

---
+++ LendingLibrary/LendingLibrary/IBag.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LendingLibrary
+{
+    public interface IBag<T> : IEnumerable<T>
+    // IEnumerable: loops over the generic or non-generic collections.
+    // GetEnumerator: get current elements from the collection.
+    {
+        /// <summary>
+        /// Add an item to the bag. <c>null</c> items are ignored.
+        /// </summary>
+        void Pack(T item);
+
+        /// <summary>
+        /// Remove the item from the bag at the given index.
+        /// </summary>
+        /// <returns>The item that was removed.</returns>
+        T Unpack(int index);
+    }
+    class IBag2<T> : IBag<T>
+    {
+        private List<T> _items = new List<T>();
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (T item in _items)
+                yield return item;
+        }
+
+        public void Pack(T item)
+        {
+            _items.Add(item);
+        }
+
+        public T Unpack(int index)
+        {
+            T item = _items[index];
+            _items.RemoveAt(index);
+            return item;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ LendingLibrary/LendingLibrary/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LendingLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Library books = new Library();
+            books.Add("Fear of Flying", "Erica", "Jong", 400);
+    }
+
+
+}
+}
